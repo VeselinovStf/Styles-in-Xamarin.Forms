3800bf4ff6e74ec3f8996c882376d2bd0345bd28
s v <chofexx@gmail.com>
2020-11-24 17:39:41 +0200

    Adding Dynamic Style

--- src/WiredBrainCoffee.ExpertApp/WiredBrainCoffee.ExpertApp/Views/CoffeeDetailPage.xaml.cs
+++ src/WiredBrainCoffee.ExpertApp/WiredBrainCoffee.ExpertApp/Views/CoffeeDetailPage.xaml.cs
@@ -6,60 +6,50 @@ using Xamarin.Forms;
 
 namespace WiredBrainCoffee.ExpertApp.Views
 {
-  [DesignTimeVisible(false)]
-  public partial class CoffeeDetailPage : ContentPage
-  {
-    private readonly CoffeeDetailViewModel _viewModel;
-
-    public CoffeeDetailPage(CoffeeDetailViewModel viewModel)
+    [DesignTimeVisible(false)]
+    public partial class CoffeeDetailPage : ContentPage
     {
-      InitializeComponent();
+        private readonly CoffeeDetailViewModel _viewModel;
 
-      BindingContext = _viewModel = viewModel;
+        public CoffeeDetailPage(CoffeeDetailViewModel viewModel)
+        {
+            InitializeComponent();
 
-      UpdateLabelProperties();
+            BindingContext = _viewModel = viewModel;
+
+            UpdateLabelProperties();
+        }
+
+        public CoffeeDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Coffee
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            _viewModel = new CoffeeDetailViewModel(item);
+            BindingContext = _viewModel;
+        }
+
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            UpdateLabelProperties();
+        }
+
+        bool _flag;
+
+        private void UpdateLabelProperties()
+        {
+            this.Resources["hedlineStyle"] = _flag ?
+            App.Current.Resources["boldLabelStyle"]
+            :
+            App.Current.Resources["italicLabelStyle"];
+
+            _flag = !_flag;
+        }
     }
-
-    public CoffeeDetailPage()
-    {
-      InitializeComponent();
-
-      var item = new Coffee
-      {
-        Text = "Item 1",
-        Description = "This is an item description."
-      };
-
-      _viewModel = new CoffeeDetailViewModel(item);
-      BindingContext = _viewModel;
-    }
-
-
-    private void Button_Clicked(object sender, System.EventArgs e)
-    {
-      UpdateLabelProperties();
-    }
-
-    bool _flag;
-
-    private void UpdateLabelProperties()
-    {
-      if (_flag)
-      {
-        titleLabel.TextColor = ingredientsLabel.TextColor =
-          Color.Gray;
-        titleLabel.FontAttributes = ingredientsLabel.FontAttributes =
-           FontAttributes.Bold | FontAttributes.Italic;
-      }
-      else
-      {
-        titleLabel.TextColor = ingredientsLabel.TextColor =
-           Color.DimGray;
-        titleLabel.FontAttributes = ingredientsLabel.FontAttributes =
-           FontAttributes.Bold;
-      }
-
-      _flag = !_flag;
-    }
-  }
 }
